bfa3fcd8baf2a7e66e4ee0498c860b6fd61e1d28
Zaskriptovan <dorozhinskiysanya@gmail.com>
2022-05-27 20:52:56 +0300

    )

--- project/create.py
+++ project/create.py
@@ -2,5 +2,3 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '123123123123123'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost/blog_db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
