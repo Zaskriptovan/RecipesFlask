1e5f38ce22ade4e8978649d0ad0cee0618a8f0f0
Zaskriptovan <103284328+Zaskriptovan@users.noreply.github.com>
2022-05-29 23:17:01 +0300

    Delete config.py

--- parser/config.py
+++
@@ -1,8 +0,0 @@
-url = 'https://www.russianfood.com/'
-
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/'
-              'avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.59'
-}
