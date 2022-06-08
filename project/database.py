15538528731a15b219618bc2efd2d56fcab60f74
Zaskriptovan <dorozhinskiysanya@gmail.com>
2022-06-08 19:13:01 +0300

    Переделал связь на many to many

--- project/database.py
+++ project/database.py
@@ -3,13 +3,17 @@ from project import app
 
 db = SQLAlchemy(app)
 
+recipe_ingredients = db.Table('recipe_ingredients',
+                              db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id')),
+                              db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredients.id')))
+
 
 class Recipes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
     text = db.Column(db.Text, nullable=False)
 
-    ingredients = db.relationship('Ingredients', backref='recipe')
+    ingredients = db.relationship('Ingredients', secondary=recipe_ingredients, backref='recipe')
 
     def __repr__(self):
         return f'id: {self.id}, title: {self.title}'
@@ -18,7 +22,6 @@ class Recipes(db.Model):
 class Ingredients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ingredient = db.Column(db.Text, nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
 
     def __repr__(self):
         return f'{self.ingredient}'
