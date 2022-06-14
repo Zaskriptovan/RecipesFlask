c85428ea6ea7828429bd04f992b808b56ae432a0
Zaskriptovan <dorozhinskiysanya@gmail.com>
2022-06-15 00:19:50 +0300

    Черновой вариант связи в базе

--- project/database.py
+++ project/database.py
@@ -3,9 +3,22 @@ from project import app
 
 db = SQLAlchemy(app)
 
-ingredients_recipes = db.Table('ingredients_recipes',
-                               db.Column('ingredient_id', db.Integer, db.ForeignKey('ingredients.id')),
-                               db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id')))
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'))
+    quantity_id = db.Column(db.Integer, db.ForeignKey('quantity.id'))
+
+    # __table_args__ = (db.UniqueConstraint(recipe_id, ingredient_id, quantity_id),)
+
+    recipe = db.relationship('Recipes', back_populates='book')
+    ingredient = db.relationship('Ingredients', back_populates='book')
+    quantity = db.relationship('Quantity', back_populates='book')
+
+    def __repr__(self):
+        return f'{self.ingredient} — {self.quantity}'
 
 
 class Recipes(db.Model):
@@ -13,17 +26,27 @@ class Recipes(db.Model):
     title = db.Column(db.Text, nullable=False)
     text = db.Column(db.Text, nullable=False)
 
-    # ingredients = db.relationship('Ingredients', secondary=recipe_ingredients, backref='recipe')
+    book = db.relationship('Book', back_populates='recipe')
 
     def __repr__(self):
-        return f'id: {self.id}, title: {self.title}'
+        return f'id: {self.id} title: {self.title}'
 
 
 class Ingredients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ingredient = db.Column(db.Text, nullable=False)
 
-    recipe = db.relationship('Recipes', secondary=ingredients_recipes, backref='ingredients')
+    book = db.relationship('Book', back_populates='ingredient')
 
     def __repr__(self):
         return f'{self.ingredient}'
+
+
+class Quantity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Text, nullable=False)
+
+    book = db.relationship('Book', back_populates='quantity')
+
+    def __repr__(self):
+        return f'{self.quantity}'
